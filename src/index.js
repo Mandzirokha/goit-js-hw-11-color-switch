653a98ab3a9ab5a1c4d593e0599cca6b70412559
Mandzirokha <hanna.mandzirokha@gmail.com>
2021-09-08 10:49:56 +0300

    hw

--- src/index.js
+++ src/index.js
@@ -9,5 +9,26 @@ const randomIntegerFromInterval = (min, max) => {
 const btnStart = document.querySelector('button[data-action="start"]');
 const btnStop = document.querySelector('button[data-action="stop"]');
 
-btnStart.addEventListener('click', startChangeColor);
-btnStop.addEventListener('click', stopChangeColor);
+const changeBackgroundColor = {
+  isActive: false,
+  intervalId: null,
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.intervalId = setInterval(() => {
+      document.body.style.background = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    this.intervalId = null;
+  },
+};
+
+btnStart.addEventListener('click', changeBackgroundColor.start.bind(changeBackgroundColor));
+btnStop.addEventListener('click', changeBackgroundColor.stop.bind(changeBackgroundColor));
